88bc8baac24ca2630c9fb7a4de49573dce33d03a
Yue Chen <fanegg@stu.xjtu.edu.cn>
2024-12-02 01:48:00 +0800

    update

--- Feat2GS/astro.config.mjs
+++ Feat2GS/astro.config.mjs
@@ -7,6 +7,8 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://fanegg.github.io',
+  base: '/Feat2GS',
   integrations: [tailwind(), icon(), mdx(), react()],
   markdown: {
     shikiConfig: {
