f0212e4725c58ba03b53fcae13d5c929f768c1d8
Yue Chen <fanegg@stu.xjtu.edu.cn>
2024-12-02 18:53:08 +0800

    compress all

--- Feat2GS/src/data/All.js
+++ Feat2GS/src/data/All.js
@@ -1,15 +1,3 @@
-import bench_clip from "../assets/All/MVimgNet_bench_lemniscate_color/clip_b16.mp4";
-import bench_dift from "../assets/All/MVimgNet_bench_lemniscate_color/dift.mp4";
-import bench_dino from "../assets/All/MVimgNet_bench_lemniscate_color/dino_b16.mp4";
-import bench_dinov2 from "../assets/All/MVimgNet_bench_lemniscate_color/dinov2_b14.mp4";
-import bench_dust3r from "../assets/All/MVimgNet_bench_lemniscate_color/dust3r.mp4";
-import bench_iuvrgb from "../assets/All/MVimgNet_bench_lemniscate_color/iuvrgb.mp4";
-import bench_mae from "../assets/All/MVimgNet_bench_lemniscate_color/mae_b16.mp4";
-import bench_mast3r from "../assets/All/MVimgNet_bench_lemniscate_color/mast3r.mp4";
-import bench_midas from "../assets/All/MVimgNet_bench_lemniscate_color/midas_l16.mp4";
-import bench_radio from "../assets/All/MVimgNet_bench_lemniscate_color/radio.mp4";
-import bench_sam from "../assets/All/MVimgNet_bench_lemniscate_color/sam_base.mp4";
-
 import caterpillar_clip from "../assets/All/Tanks_Caterpillar_arc_color/clip_b16.mp4";
 import caterpillar_dift from "../assets/All/Tanks_Caterpillar_arc_color/dift.mp4";
 import caterpillar_dino from "../assets/All/Tanks_Caterpillar_arc_color/dino_b16.mp4";
@@ -22,73 +10,20 @@ import caterpillar_midas from "../assets/All/Tanks_Caterpillar_arc_color/midas_l
 import caterpillar_radio from "../assets/All/Tanks_Caterpillar_arc_color/radio.mp4";
 import caterpillar_sam from "../assets/All/Tanks_Caterpillar_arc_color/sam_base.mp4";
 
+
+import bench_clip from "../assets/All/MVimgNet_bench_lemniscate_color/clip_b16.mp4";
+import bench_dift from "../assets/All/MVimgNet_bench_lemniscate_color/dift.mp4";
+import bench_dino from "../assets/All/MVimgNet_bench_lemniscate_color/dino_b16.mp4";
+import bench_dinov2 from "../assets/All/MVimgNet_bench_lemniscate_color/dinov2_b14.mp4";
+import bench_dust3r from "../assets/All/MVimgNet_bench_lemniscate_color/dust3r.mp4";
+import bench_iuvrgb from "../assets/All/MVimgNet_bench_lemniscate_color/iuvrgb.mp4";
+import bench_mae from "../assets/All/MVimgNet_bench_lemniscate_color/mae_b16.mp4";
+import bench_mast3r from "../assets/All/MVimgNet_bench_lemniscate_color/mast3r.mp4";
+import bench_midas from "../assets/All/MVimgNet_bench_lemniscate_color/midas_l16.mp4";
+import bench_radio from "../assets/All/MVimgNet_bench_lemniscate_color/radio.mp4";
+import bench_sam from "../assets/All/MVimgNet_bench_lemniscate_color/sam_base.mp4";
+
 export const AllData = [
-  {
-    source: bench_dust3r,
-    label: "DUSt3R",
-    scene: "bench",
-    color: "#B2CBC2"
-  },
-  {
-    source: bench_mast3r,
-    label: "MASt3R",
-    scene: "bench",
-    color: "#BBC990"
-  },
-  {
-    source: bench_midas,
-    label: "MiDaS",
-    scene: "bench",
-    color: "#6B859E"
-  },
-  {
-    source: bench_dinov2,
-    label: "DINOv2",
-    scene: "bench",
-    color: "#B45342"
-  },
-  {
-    source: bench_dino,
-    label: "DINO",
-    scene: "bench",
-    color: "#DCAC99"
-  },
-  {
-    source: bench_sam,
-    label: "SAM",
-    scene: "bench",
-    color: "#6F936B"
-  },
-  {
-    source: bench_clip,
-    label: "CLIP",
-    scene: "bench",
-    color: "#EBA062"
-  },
-  {
-    source: bench_radio,
-    label: "RADIO",
-    scene: "bench",
-    color: "#FED273"
-  },
-  {
-    source: bench_mae,
-    label: "MAE",
-    scene: "bench",
-    color: "#9A8EB4"
-  },
-  {
-    source: bench_dift,
-    label: "SD",
-    scene: "bench",
-    color: "#706052"
-  },
-  {
-    source: bench_iuvrgb,
-    label: "IUVRGB",
-    scene: "bench",
-    color: "#E9E5E5"
-  },
   {
     source: caterpillar_dust3r,
     label: "DUSt3R",
@@ -153,6 +88,72 @@ export const AllData = [
     source: caterpillar_iuvrgb,
     label: "IUVRGB",
     scene: "Caterpillar",
+    color: "#E9E5E5"
+  }, 
+  {
+    source: bench_dust3r,
+    label: "DUSt3R",
+    scene: "bench",
+    color: "#B2CBC2"
+  },
+  {
+    source: bench_mast3r,
+    label: "MASt3R",
+    scene: "bench",
+    color: "#BBC990"
+  },
+  {
+    source: bench_midas,
+    label: "MiDaS",
+    scene: "bench",
+    color: "#6B859E"
+  },
+  {
+    source: bench_dinov2,
+    label: "DINOv2",
+    scene: "bench",
+    color: "#B45342"
+  },
+  {
+    source: bench_dino,
+    label: "DINO",
+    scene: "bench",
+    color: "#DCAC99"
+  },
+  {
+    source: bench_sam,
+    label: "SAM",
+    scene: "bench",
+    color: "#6F936B"
+  },
+  {
+    source: bench_clip,
+    label: "CLIP",
+    scene: "bench",
+    color: "#EBA062"
+  },
+  {
+    source: bench_radio,
+    label: "RADIO",
+    scene: "bench",
+    color: "#FED273"
+  },
+  {
+    source: bench_mae,
+    label: "MAE",
+    scene: "bench",
+    color: "#9A8EB4"
+  },
+  {
+    source: bench_dift,
+    label: "SD",
+    scene: "bench",
+    color: "#706052"
+  },
+  {
+    source: bench_iuvrgb,
+    label: "IUVRGB",
+    scene: "bench",
     color: "#E9E5E5"
   },
 ];
