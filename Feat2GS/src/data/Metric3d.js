d128292919fbdf2391ef86b1e385d75f7852c2c6
Yue Chen <fanegg@stu.xjtu.edu.cn>
2024-12-09 17:43:54 +0800

    Update Metric3d.js

--- Feat2GS/src/data/Metric3d.js
+++ Feat2GS/src/data/Metric3d.js
@@ -23,72 +23,6 @@ import dift_scan48 from "../assets/metric3d/dift_DTU_scan48_spiral.mp4";
 import iuvrgb_scan48 from "../assets/metric3d/iuvrgb_DTU_scan48_spiral.mp4";
 
 export const Metric3dData = [
-  {
-    source: dust3r_scan1,
-    label: "DUSt3R",
-    scene: "Scan1",
-    color: "#B2CBC2"
-  },
-  {
-    source: mast3r_scan1,
-    label: "MASt3R",
-    scene: "Scan1",
-    color: "#BBC990"
-  },
-  {
-    source: midas_scan1,
-    label: "MiDaS",
-    scene: "Scan1",
-    color: "#6B859E"
-  },
-  {
-    source: dinov2_scan1,
-    label: "DINOv2",
-    scene: "Scan1",
-    color: "#B45342"
-  },
-  {
-    source: dino_scan1,
-    label: "DINO",
-    scene: "Scan1",
-    color: "#DCAC99"
-  },
-  {
-    source: sam_scan1,
-    label: "SAM",
-    scene: "Scan1",
-    color: "#6F936B"
-  },
-  {
-    source: clip_scan1,
-    label: "CLIP",
-    scene: "Scan1",
-    color: "#EBA062"
-  },
-  {
-    source: radio_scan1,
-    label: "RADIO",
-    scene: "Scan1",
-    color: "#FED273"
-  },
-  {
-    source: mae_scan1,
-    label: "MAE",
-    scene: "Scan1",
-    color: "#9A8EB4"
-  },
-  {
-    source: dift_scan1,
-    label: "SD",
-    scene: "Scan1",
-    color: "#706052"
-  },
-  {
-    source: iuvrgb_scan1,
-    label: "IUVRGB",
-    scene: "Scan1",
-    color: "#E9E5E5"
-  },
 
   {
     source: dust3r_scan48,
@@ -154,6 +88,73 @@ export const Metric3dData = [
     source: iuvrgb_scan48,
     label: "IUVRGB",
     scene: "Scan48",
+    color: "#E9E5E5"
+  },
+
+  {
+    source: dust3r_scan1,
+    label: "DUSt3R",
+    scene: "Scan1",
+    color: "#B2CBC2"
+  },
+  {
+    source: mast3r_scan1,
+    label: "MASt3R",
+    scene: "Scan1",
+    color: "#BBC990"
+  },
+  {
+    source: midas_scan1,
+    label: "MiDaS",
+    scene: "Scan1",
+    color: "#6B859E"
+  },
+  {
+    source: dinov2_scan1,
+    label: "DINOv2",
+    scene: "Scan1",
+    color: "#B45342"
+  },
+  {
+    source: dino_scan1,
+    label: "DINO",
+    scene: "Scan1",
+    color: "#DCAC99"
+  },
+  {
+    source: sam_scan1,
+    label: "SAM",
+    scene: "Scan1",
+    color: "#6F936B"
+  },
+  {
+    source: clip_scan1,
+    label: "CLIP",
+    scene: "Scan1",
+    color: "#EBA062"
+  },
+  {
+    source: radio_scan1,
+    label: "RADIO",
+    scene: "Scan1",
+    color: "#FED273"
+  },
+  {
+    source: mae_scan1,
+    label: "MAE",
+    scene: "Scan1",
+    color: "#9A8EB4"
+  },
+  {
+    source: dift_scan1,
+    label: "SD",
+    scene: "Scan1",
+    color: "#706052"
+  },
+  {
+    source: iuvrgb_scan1,
+    label: "IUVRGB",
+    scene: "Scan1",
     color: "#E9E5E5"
   }
 ];
